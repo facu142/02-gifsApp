e024f3dfa429661d619160954a4adc53af12edf8
Facundo Montenegro <facumonte29@gmail.com>
2021-03-27 01:11:33 -0300

    agrega tipado a peticion http
    se creo la carpeta interface con la finalidad de almacenar la interface de los gifs recibidos de la API, esta misma obtenida a través de  quicktype

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,31 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SearchGIFResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private apyKey:string = 'oohQ5izopuIERdF5fOwiShmc4h33tHzc';
+  private apyKey: string = 'oohQ5izopuIERdF5fOwiShmc4h33tHzc';
   private _historial: string[] = [];
-  
+
+  public resultados : Gif[] = [];
+
   get historial() {
     return [...this._historial];
   }
-  
+
+  constructor(private http:HttpClient) { }
+
   buscarGifs(query: string = '') {
-    
+
     query = query.trim().toLowerCase();
 
     if (!this._historial.includes(query)) {
 
       this._historial.unshift(query);
-      
+
       this._historial = this._historial.splice(0, 10);
 
     }
 
+    this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=oohQ5izopuIERdF5fOwiShmc4h33tHzc&q=${query}&limit=10`)
+    .subscribe( (resp : SearchGIFResponse) => {
+      console.log(resp);
+      this.resultados = resp.data;
+    })
 
+    
     console.log(this._historial);
+
   }
 
 }
