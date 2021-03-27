e4b0663357dd730326c60d3b279dd799f74724bb
Facundo Montenegro <facumonte29@gmail.com>
2021-03-27 01:31:29 -0300

    agrega localStorage
    Se añadió un local storage en la cual guarda las ultimas búsquedas

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,33 +10,36 @@ export class GifsService {
   private apyKey: string = 'oohQ5izopuIERdF5fOwiShmc4h33tHzc';
   private _historial: string[] = [];
 
-  public resultados : Gif[] = [];
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
   }
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [] ;
+    
+
+  }
 
   buscarGifs(query: string = '') {
 
     query = query.trim().toLowerCase();
 
     if (!this._historial.includes(query)) {
-
       this._historial.unshift(query);
-
       this._historial = this._historial.splice(0, 10);
-
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
     this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=oohQ5izopuIERdF5fOwiShmc4h33tHzc&q=${query}&limit=10`)
-    .subscribe( (resp : SearchGIFResponse) => {
-      console.log(resp);
-      this.resultados = resp.data;
-    })
+      .subscribe((resp: SearchGIFResponse) => {
+        console.log(resp);
+        this.resultados = resp.data;
+      })
 
-    
+
     console.log(this._historial);
 
   }
